9d428baa5e6ed1c7fb70fa7c46e5f0c58b23d105
NikolayMelon <81904080+NikolayMelon@users.noreply.github.com>
2021-07-04 12:27:03 +0300

    Main code added

--- laby.py
+++ laby.py
@@ -61,64 +61,136 @@ def location_extractor(mapFileText,loc):
 
 start_location = location_extractor(mapFileText,start)
 end_location = location_extractor(mapFileText,end)
+player = '_Ω_'
+def game_menu(player):
+    player_location = start_location.copy()
+    on_off = input('_')
+    if on_off == 'on':
+        game_on = False
+        print('Welcome to LABY by Pygaming\n\nEnter 1 to choose map, enter 2 to choose skin, enter 3 to exit')
+        choice = input('Time to make the choice! ')
+        if choice == '2':
+            print('1 for Omega(_Ω_), 2 for Little Boy(_I_), 3 for Fat Man(_O_)')
+            skin_choice = input('Time to make the choice! ')
+            if skin_choice == '1':
+                player = '_Ω_'
+                game_menu(player)
+            elif skin_choice == '2':
+                player = '_I_'
+                game_menu(player)
+            elif skin_choice == '3':
+                player = '_O_'
+                game_menu(player)
+        elif choice == '1':
+            win = False
+            print(f'{player} is player, П is exit, use w/a/s/d keys to move (Enter w to go up, etc).')
+            game_on = True
+            while game_on == True:
+                labyprinter(cutted_labyrinth)
+                put = input()
+                os.system('clear')
+                if put == 's':
+                    print('BLINK!')
+                    if cutted_labyrinth[player_location[0]+1][player_location[1]] == '___':
+                        cutted_labyrinth[player_location[0]][player_location[1]] = '___' 
+                        player_location[0] = player_location[0] + 1 
+                        cutted_labyrinth[player_location[0]][player_location[1]] = player 
+                    elif cutted_labyrinth[player_location[0]+1][player_location[1]] ==  '_П_':
+                        print('YOU WON')
+                        break
+                elif put == 'w':
+                    print('BLINK!')
+                    if cutted_labyrinth[player_location[0]-1][player_location[1]] == '___':
+                        cutted_labyrinth[player_location[0]][player_location[1]] = '___'
+                        player_location[0] = player_location[0] - 1
+                        cutted_labyrinth[player_location[0]][player_location[1]] = player
+                    elif cutted_labyrinth[player_location[0]-1][player_location[1]] == '_П_':
+                        print('YOU WON')
+                        break
+                elif put == 'a':
+                    print('BLINK!')
+                    if cutted_labyrinth[player_location[0]][player_location[1]-1] == '___':
+                        cutted_labyrinth[player_location[0]][player_location[1]] = '___' 
+                        player_location[1] = player_location[1] - 1 
+                        cutted_labyrinth[player_location[0]][player_location[1]] = player
+                    elif cutted_labyrinth[player_location[0]][player_location[1]-1] == '_П_':
+                        print('YOU WIN')
+                        break
+                elif put == 'd':
+                    print('BLINK!')
+                    if cutted_labyrinth[player_location[0]][player_location[1]+1] == '___':
+                        cutted_labyrinth[player_location[0]][player_location[1]] = '___'
+                        player_location[1] = player_location[1] + 1
+                        cutted_labyrinth[player_location[0]][player_location[1]] = player
+                    elif cutted_labyrinth[player_location[0]][player_location[1]+1] == '_П_':
+                        print('YOU WON')
+                        break
+
+
 
 #print(f'player coordinates = {start_location}')
 #print(f'exit coordinates = {end_location}')
 player_location = start_location.copy()
-game_on = False
-player = '_Ω_'
-print('Welcome to LABY by Pygaming\n\nEnter 1 to choose map, enter 2 to choose skin, enter 3 to exit')
-choice = input('Time to make the choice! ')
-if choice == '2':
-    print('1 for Omega(_Ω_), 2 for Little Boy(_I_), 3 for Fat Man(_O_)')
-    skin_choice = input('Time to make the choice! ')
-    if skin_choice == '1':
-        player = '_Ω_'
-    elif skin_choice == '2':
-        player = '_I_'
-    elif skin_choice == '3':
-        player = '_O_'
-elif choice == '1':
-    win = False
-    print(f'{player} is player, П is exit, use w/a/s/d keys to move (Enter w to go up, etc).')
-    game_on = True
-while game_on == True:
-    labyprinter(cutted_labyrinth)
-    put = input()
-    os.system('clear')
-    if put == 's':
-        print('BLINK!')
-        if cutted_labyrinth[player_location[0]+1][player_location[1]] == '___':
-            cutted_labyrinth[player_location[0]][player_location[1]] = '___' 
-            player_location[0] = player_location[0] + 1 
-            cutted_labyrinth[player_location[0]][player_location[1]] = player 
-        elif cutted_labyrinth[player_location[0]+1][player_location[1]] ==  '_П_':
-            print('YOU WON')
-            break
-    elif put == 'w':
-        print('BLINK!')
-        if cutted_labyrinth[player_location[0]-1][player_location[1]] == '___':
-            cutted_labyrinth[player_location[0]][player_location[1]] = '___'
-            player_location[0] = player_location[0] - 1
-            cutted_labyrinth[player_location[0]][player_location[1]] = player
-        elif cutted_labyrinth[player_location[0]-1][player_location[1]] == '_П_':
-            print('YOU WON')
-            break
-    elif put == 'a':
-        print('BLINK!')
-        if cutted_labyrinth[player_location[0]][player_location[1]-1] == '___':
-            cutted_labyrinth[player_location[0]][player_location[1]] = '___' 
-            player_location[1] = player_location[1] - 1 
-            cutted_labyrinth[player_location[0]][player_location[1]] = player
-        elif cutted_labyrinth[player_location[0]][player_location[1]-1] == '_П_':
-            print('YOU WIN')
-            break
-    elif put == 'd':
-        print('BLINK!')
-        if cutted_labyrinth[player_location[0]][player_location[1]+1] == '___':
-            cutted_labyrinth[player_location[0]][player_location[1]] = '___'
-            player_location[1] = player_location[1] + 1
-            cutted_labyrinth[player_location[0]][player_location[1]] = player
-        elif cutted_labyrinth[player_location[0]][player_location[1]+1] == '_П_':
-            print('YOU WON')
-            break
+on_off = input('_')
+if on_off == 'on':
+    game_on = False
+    print('Welcome to LABY by Pygaming\n\nEnter 1 to choose map, enter 2 to choose skin, enter 3 to exit')
+    choice = input('Time to make the choice! ')
+    if choice == '2':
+        print('1 for Omega(_Ω_), 2 for Little Boy(_I_), 3 for Fat Man(_O_)')
+        skin_choice = input('Time to make the choice! ')
+        if skin_choice == '1':
+            player = '_Ω_'
+            game_menu(player)
+        elif skin_choice == '2':
+            player = '_I_'
+            game_menu(player)
+        elif skin_choice == '3':
+            player = '_O_'
+            game_menu(player)
+    elif choice == '1':
+        win = False
+        print(f'{player} is player, П is exit, use w/a/s/d keys to move (Enter w to go up, etc).')
+        game_on = True
+        while game_on == True:
+            labyprinter(cutted_labyrinth)
+            put = input()
+            os.system('clear')
+            if put == 's':
+                print('BLINK!')
+                if cutted_labyrinth[player_location[0]+1][player_location[1]] == '___':
+                    cutted_labyrinth[player_location[0]][player_location[1]] = '___' 
+                    player_location[0] = player_location[0] + 1 
+                    cutted_labyrinth[player_location[0]][player_location[1]] = player 
+                elif cutted_labyrinth[player_location[0]+1][player_location[1]] ==  '_П_':
+                    print('YOU WON')
+                    break
+            elif put == 'w':
+                print('BLINK!')
+                if cutted_labyrinth[player_location[0]-1][player_location[1]] == '___':
+                    cutted_labyrinth[player_location[0]][player_location[1]] = '___'
+                    player_location[0] = player_location[0] - 1
+                    cutted_labyrinth[player_location[0]][player_location[1]] = player
+                elif cutted_labyrinth[player_location[0]-1][player_location[1]] == '_П_':
+                    print('YOU WON')
+                    break
+            elif put == 'a':
+                print('BLINK!')
+                if cutted_labyrinth[player_location[0]][player_location[1]-1] == '___':
+                    cutted_labyrinth[player_location[0]][player_location[1]] = '___' 
+                    player_location[1] = player_location[1] - 1 
+                    cutted_labyrinth[player_location[0]][player_location[1]] = player
+                elif cutted_labyrinth[player_location[0]][player_location[1]-1] == '_П_':
+                    print('YOU WIN')
+                    break
+            elif put == 'd':
+                print('BLINK!')
+                if cutted_labyrinth[player_location[0]][player_location[1]+1] == '___':
+                    cutted_labyrinth[player_location[0]][player_location[1]] = '___'
+                    player_location[1] = player_location[1] + 1
+                    cutted_labyrinth[player_location[0]][player_location[1]] = player
+                elif cutted_labyrinth[player_location[0]][player_location[1]+1] == '_П_':
+                    print('YOU WON')
+                    break
+
+#Homevrk = 'get all of this on git, comment and etc, remember the git commands'
